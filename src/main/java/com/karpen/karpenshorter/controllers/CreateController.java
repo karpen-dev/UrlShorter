6f7456950f0d03f49c9d4ab6b8c6d1ad8e5e6494
karpen <ghhvvfghvg44@gmail.com>
2025-02-18 16:51:11 +0300

    fix controllers

--- src/main/java/com/karpen/karpenshorter/controllers/CreateController.java
+++ src/main/java/com/karpen/karpenshorter/controllers/CreateController.java
@@ -4,7 +4,7 @@ import com.karpen.karpenshorter.service.UrlShortService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class CreateController {
 
     private UrlShortService shortService = new UrlShortService();
