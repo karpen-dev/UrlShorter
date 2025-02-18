3bed28b11e75700edf1fd36afab8cbd2aa97cc2b
karpen <ghhvvfghvg44@gmail.com>
2025-02-18 16:39:44 +0300

    fix controllers

--- src/main/java/com/karpen/karpenshorter/controllers/GetController.java
+++ src/main/java/com/karpen/karpenshorter/controllers/GetController.java
@@ -4,8 +4,9 @@ import com.karpen.karpenshorter.service.UrlShortService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class GetController {
 
     private UrlShortService shortService = new UrlShortService();
--- src/main/java/com/karpen/karpenshorter/controllers/MainController.java
+++ src/main/java/com/karpen/karpenshorter/controllers/MainController.java
@@ -3,12 +3,12 @@ package com.karpen.karpenshorter.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/")
+@RestController
 public class MainController {
 
-    @GetMapping
+    @GetMapping("/")
     public String index(){
         return "redirect://karpendev.ru/short";
     }
